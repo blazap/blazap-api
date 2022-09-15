231ebb61d881e7e919a7900eb3b1a2cd6aec0508
itroeix <deividgeorge2017@gmail.com>
2022-09-15 17:58:22 +0200

    All paths implemented

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# blazap-api
+# Blazap API
+Improvement of code organization it will get better soon.
+
+And the organization of the documentation is also will get better soon.
+
+Paths:
+
+`/apps`
+
+`/apps/category/(category here)`
+
+`/apps/search/(search anything here)`
+
+`/apps/id/(id here)`
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config({ path: './.env' });
+const port = process.env.PORT || 8383;
+const app = express();
+app.use(cors());
+
+app.get('/apps', async (req, res) => {
+    const response = await axios.get(process.env.DATABASE_URL);
+    const sorted = response.data.sort((a, b) => {
+        return b.id - a.id;
+    })
+    res.send(sorted)
+})
+
+app.get('/apps/category/:category', async (req, res) => {
+    const response = await axios.get(process.env.DATABASE_URL);
+    const data = response.data
+    const param = req.params.category
+    console.log(param)
+    const find = response.data.filter(d => d.category === param);
+    res.json(find);
+})
+
+app.get('/apps/search/:search', async (req, res) => {
+    const response = await axios.get(process.env.DATABASE_URL);
+    const data = response.data
+    const searchString = req.params.search
+    searchRegExp = new RegExp(searchString, 'i');
+    const find = await data.filter(function (e) {
+        return searchRegExp.test(e.lower);
+    });
+    res.json(find);
+})
+
+app.get('/apps/id/:id', async (req, res) => {
+    const response = await axios.get(process.env.DATABASE_URL);
+    const data = response.data
+    const param = req.params.id
+    const find = await data.find(function (e) {
+        return e.id == param;
+    })
+    res.json(find);
+})
+
+
+
+app.listen(port);
